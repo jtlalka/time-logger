c4650ef368a4386f8ae80814ecf0229305d50430
Jakub Tlalka <jakub@tlalka.net>
2016-02-15 21:09:46 +0100

    Replaced time input to select input in options view

--- dist/app/js/controllers/optionsController.js
+++ dist/app/js/controllers/optionsController.js
@@ -7,8 +7,6 @@ angular.module('timeLogger')
 
         $scope.data = {
             timePerDay: [],
-            minTimePerDay: new Date(commonService.toMillisecond(0)),
-            maxTimePerDay: new Date(commonService.toMillisecond(0, 0, 12)),
             timeOptions: optionsDao.getTimeOptions()
         };
 
@@ -21,24 +19,16 @@ angular.module('timeLogger')
             $scope.display.header = $routeParams.router;
 
             optionsDao.getOptions().then(function(options) {
-                $scope.options = initOptionsObject(options);
+                $scope.options = options;
                 loggerService.trace('OptionsController: init data.', options);
             });
-        };
-
-        var initOptionsObject = function(options) {
-            for (var i = 0, len = options.timePerDay.length; i < len; i++) {
-                $scope.data.timePerDay[i] = commonService.toUTCDate(options.timePerDay[i]);
-            }
-            return options;
         };
 
         $scope.checkWorkingTime = function() {
             $scope.options.timePerWeek = 0;
 
             for (var i = 0, len = $scope.options.timePerDay.length; i < len; i++) {
-                if ($scope.data.timePerDay[i]) {
-                    $scope.options.timePerDay[i] = commonService.dateToTime($scope.data.timePerDay[i]);
+                if ($scope.options.timePerDay[i]) {
                     $scope.options.timePerWeek += $scope.options.timePerDay[i];
                 } else {
                     $scope.options.timePerDay[i] = 0;
@@ -53,7 +43,7 @@ angular.module('timeLogger')
                 loggerService.info('OptionsController: update options.', options);
 
                 $timeout(function() {
-                    $scope.options = initOptionsObject(options);
+                    $scope.options = options;
                     $scope.display.update = false;
                     $scope.optionsForm.$setPristine();
                 }, 200);
@@ -64,7 +54,7 @@ angular.module('timeLogger')
             var hourInMillisecond = commonService.toMillisecond(0, 0, 1);
             var hours = Math.floor(time / hourInMillisecond);
             var minutes = Math.floor(time % hourInMillisecond / 60 / 1000);
-            return hours + (minutes < 10 ? ' : 0' + minutes : ' : ' + minutes);
+            return ('00' + hours).substr(-2) + ':' + ('00' + minutes).substr(-2);
         };
 
         $scope.showActivityHelp = function() {
